931e825ac93e4afef024759e64aff1cbf83f8eda
1313 <sdwdad2@dasd.ads>
2022-03-31 17:31:46 +0300

    add article form components

---
+++ src/views/CreateArticle.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    CreateArticle
+    <mcv-article-form
+      :initial-values="initialValues"
+      :errors="validationErrors"
+      :is-submitting="isSubmitting"
+      @articleSubmit="onSubmit"
+    />
+  </div>
+</template>
+
+<script>
+import McvArticleForm from "@/components/ArticleForm.vue";
+export default {
+  name: "McvCreateArticle",
+
+  components: {
+    McvArticleForm,
+  },
+  data() {
+    return {
+      initialValues: {
+        title: "",
+        description: "",
+        body: "",
+        tagList: [],
+      },
+      validationErrors: null,
+      isSubmitting: false,
+    };
+  },
+  methods: {
+    onSubmit(data) {
+      console.log("create form", data);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
